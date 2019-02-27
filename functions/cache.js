191e44dd848a17a7b5dba9b53c255d220b0d7eea
Joseph Ting <josephting@users.noreply.github.com>
2019-02-27 15:21:17 +0900

    Add cache test function

---
+++ functions/cache.js
@@ -0,0 +1,21 @@
+const ttl = 60 * 60 * 1000 // 1 minute in miliseconds
+
+let lastUpdate = 0
+let data = {}
+
+exports.handler = (event, context, callback) => {
+  if (Object.keys(data).length === 0 || Date.now() - lastUpdate >= ttl) {
+    console.log('Updating data...')
+    let now = Date.now()
+    data.lastUpdate = now
+    lastUpdate = now
+  }
+
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+}
